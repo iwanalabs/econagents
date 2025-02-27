c5b6c5b6d87c8dad7e695dd19fbd9950912a51d7
Dylan Castillo <dylanjcastillo@gmail.com>
2025-02-27 10:42:50 +0000

    Add base prisoner agent

---
+++ examples/prisoner/agents.py
@@ -0,0 +1,8 @@
+from econagents.core.agent import Agent
+
+
+class PrisonerAgent(Agent):
+    """Base class for prisoner agents in the Prisoner's Dilemma game."""
+
+    role = 1
+    name = "Prisoner"
